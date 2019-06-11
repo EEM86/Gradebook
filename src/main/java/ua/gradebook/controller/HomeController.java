bbef9ceae9eb70fce2a3cd8bf1df5cde8f0f5844
EEM86 <eevgeniy@gmail.com>
2019-06-11 18:42:55 +0300

    Added person to session using Spring AOP

--- src/main/java/ua/gradebook/controller/HomeController.java
+++ src/main/java/ua/gradebook/controller/HomeController.java
@@ -1,39 +1,23 @@
 package ua.gradebook.controller;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import ua.gradebook.model.beans.Person;
-import ua.gradebook.service.PersonService;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
-@SessionAttributes("loggedPerson")
 public class HomeController {
     private static final Logger logger = Logger.getLogger(HomeController.class);
 
-    @Autowired
-    PersonService personService;
-
     /**
-     * Main page with saved logged Person object in the session.
-     * @param session current client-server session
+     * Main page.
      * @param model current model
      * @return name of a jsp page
      */
     @GetMapping(value="/")
-    public String Main (HttpSession session, Model model) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Person person = (Person) personService.findByLogin(user.getUsername());
-        session.setAttribute("loggedPerson", person);
+    public String Main (Model model) {
         logger.info("index load");
         return "index";
     }
